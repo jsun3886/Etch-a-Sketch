abb35a50951e6670d0d796873db654385a0dfe52
jsun3886 <jsunday86@gmail.com>
2022-10-06 12:52:06 -0400

    Adds button to index and functionality in .js to create a sized board
    Creates the functionality to create a board between 1 and 100 that has
    the same functionallity as the original board. It still needs to be edited to
    remove the old board.

--- etchASketch.js
+++ etchASketch.js
@@ -1,19 +1,48 @@
 page = document.querySelector(".main");
 
-for (row=0; row<16;row++){
-    divRow= document.createElement('div')
-    divRow.setAttribute('class','row')
-    for (col=0; col<16; col++){
-        square = document.createElement('div');
-        square.setAttribute('class', "square");
-        square.textContent='square';
-        square.addEventListener('mouseover',colorsquare)
-        divRow.appendChild(square);
-    }
-    page.appendChild(divRow);
-}
 
+function createBoard(size){
+    page = document.querySelector(".main");
+
+    for (row=0; row<size;row++){
+        divRow= document.createElement('div')
+        divRow.setAttribute('class','row')
+        high= Math.round(800/size);
+        divRow.style.height =`${high}px`
+        for (col=0; col<size; col++){
+            square = document.createElement('div');
+            square.setAttribute('class', "square");
+            
+            square.addEventListener('mouseover',colorsquare);
+            square.style.height=`${high}px`;
+            square.style.width=`${high}px`;
+            divRow.appendChild(square);
+        }
+        page.appendChild(divRow);
+    }
+}
 function colorsquare(){
     this.classList.add('colored');
 
 }
+
+function resizeBoard(){
+    numberSquares=prompt("enter the size of the board")
+        while(isNaN(parseInt(numberSquares))){
+            numberSquares=prompt("enter the size of the board")
+        }
+        while(parseInt(numberSquares)>100 || parseInt(numberSquares)<0){
+            numberSquares=prompt("please enter a size between 1 and 100")
+        }
+    createBoard(numberSquares);
+    }
+
+
+
+
+
+
+setSizeButton= document.querySelector('.numSquaresButton')
+setSizeButton.addEventListener('click',resizeBoard)
+
+createBoard(16)
