e68621d7541f3d3160a728393cdaa49369d27fdf
jsun3886 <jsunday86@gmail.com>
2022-10-06 21:42:46 -0400

    Makes the set number of square button work adds animate
    sets the number of squares to the inputed prompt after the button push
    it adds a deleteing class to the squares and animates a small shake before
    the new board pops up.

--- etchASketch.js
+++ etchASketch.js
@@ -7,6 +7,7 @@ function createBoard(size){
     for (row=0; row<size;row++){
         divRow= document.createElement('div')
         divRow.setAttribute('class','row')
+        divRow.addEventListener('transitionend',removetransmition);
         high= Math.round(800/size);
         divRow.style.height =`${high}px`
         for (col=0; col<size; col++){
@@ -14,8 +15,11 @@ function createBoard(size){
             square.setAttribute('class', "square");
             
             square.addEventListener('mouseover',colorsquare);
+            square.addEventListener('transitionend',removetransmition);
             square.style.height=`${high}px`;
             square.style.width=`${high}px`;
+            
+
             divRow.appendChild(square);
         }
         page.appendChild(divRow);
@@ -34,15 +38,30 @@ function resizeBoard(){
         while(parseInt(numberSquares)>100 || parseInt(numberSquares)<0){
             numberSquares=prompt("please enter a size between 1 and 100")
         }
+    deleteboard();
     createBoard(numberSquares);
     }
 
 
+function deleteboard(){
+board = document.querySelectorAll('.square');
 
-
+board.forEach(element => {element.classList.add('deleteing');
+    
+});
+}
 
 
 setSizeButton= document.querySelector('.numSquaresButton')
 setSizeButton.addEventListener('click',resizeBoard)
+
+function removetransmition(e){
+    if(e.propertyName != 'transform'){return ;}
+    this.remove();
+  }
+  
+
+
+
 
 createBoard(16)
